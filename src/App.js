09217a55b9cb2bc0b5a578be5f5670c9e881fb8a
Brian <briantechdom@gmail.com>
2022-01-15 09:20:05 -0800

    Setup search async function and useState hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-// import "./styles.css";
+import React, {useState} from 'react';
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>PWA Weather App</h1>
-    </div>
-  );
-}
+import {fetchWeather} from './api/fetchWeather';
+import './App.css';
+
+const App = () => {
+  // set the variable and function for useState
+  const [query, setQuery] = useState('');
+
+  const search = async (e) => {
+    // if keyboard  is enter
+    if(e.key === 'Enter') {
+      // pass the query variable from our useState above
+      const data = await fetchWeather(query)
+
+      console.log(data);
+    }
+  }
+
+  return(
+  <div className="main-container">
+    {/* value and onChange below need to be coming from the state using the useState hook*/}
+    <input
+      type="text"
+      className="search"
+      placeholder="Search..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      onKeyPress={search}
+    />
+  </div>
+  )}
+
+  export default App;
